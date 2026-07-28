b10d2ca84bc84e3e5bfb7d51ab34a28e023a71a1
Sebastian Nicoras <sebastiano.nicoras@gmail.com>
2026-07-28 13:28:06 +0300

    reduce mobile gap and side margin

--- src/components/SelectedWork.tsx
+++ src/components/SelectedWork.tsx
@@ -14,7 +14,7 @@ export default function SelectedWork() {
   if (isMobile) {
     return (
       <section id="portfolio" className="w-full">
-        <div style={{ display: "flex", flexDirection: "column", gap: "48px", paddingTop: "16px", paddingBottom: "16px", paddingLeft: "16px", paddingRight: "16px" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "32px", paddingTop: "16px", paddingBottom: "16px", paddingLeft: "10px", paddingRight: "10px" }}>
           {allImages.map((src, i) => (
             <RevealOnScroll key={src}>
               <LightboxTrigger images={allImages} index={i}>
